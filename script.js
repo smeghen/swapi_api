55c5dd2d30b944c7448e15f6fdeb3b2cf66b335e
smeghen <smeghen@gmail.com>
2022-02-13 19:31:40 +0000

    LoadTable function added to populate the table with the data gathered from the api

--- script.js
+++ script.js
@@ -12,12 +12,29 @@ async function getData() {
     console.log(people.length)
     console.log(people)
 
+    // Function to load api data onto table
+    function loadTable(people){
+        var table = document.getElementById("tbody");
+        for (i = 0; i < people.length; i++){
+            var row = `<tr>
+                        <td>${people[i].name}</td>
+                        <td>${people[i].height}</td>
+                        <td>${people[i].mass}</td>
+                        <td>${people[i].created}</td>
+
+            </tr>
+            `
+            table.innerHTML += row
+        }
+    }
+
     // function to hide loader spinner and display table when data is finised loading from api
     function hideloader() {
         document.getElementById('loading').style.display = 'none';
         document.getElementById('table').style.display = 'block';
     }
 
+    loadTable(people)   
     hideloader();
 }
   
