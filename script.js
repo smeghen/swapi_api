a1be464cf2a52f7e6f8acc70816e091cbf78f18b
smeghen <smeghen@gmail.com>
2022-02-13 20:51:45 +0000

    Added Filter function to search the data table by name

--- script.js
+++ script.js
@@ -38,5 +38,28 @@ async function getData() {
     loadTable(people)   
     hideloader();
 }
+
+function Filter() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 1; i < tr.length; i++) {
+        // Hide the row initially.
+        tr[i].style.display = "none";
+    
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+          cell = tr[i].getElementsByTagName("td")[j];
+          if (cell) {
+            if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+              break;
+            } 
+          }
+        }
+    }
+}
   
 getData()
